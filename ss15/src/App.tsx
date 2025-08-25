55b21146e2a6c50f44d81cdbb1dcc8748673fca6
Phanh2910 <phanphuocanh97@gmail.com>
2025-08-25 20:56:28 +0700

    Update App.tsx

--- ss15/src/App.tsx
+++ ss15/src/App.tsx
@@ -1,7 +1,11 @@
 import { useState } from 'react'
 // import ex1 from "../src/components/ex1";
 // import  Ex2 from "../src/components/ex2";
-import Ex3 from "../src/components/Ex3";
+// import Ex3 from "../src/components/Ex3";
+// import ex4 from "../src/components/ex4"
+// import DetailPost from "../src/components/SS15_B6/DetailPost"
+import Ex7 from "../src/components/ex7"
+// import Ex8 from "../src/components/Ex8";
 import './App.css'
 
 function App() {
@@ -9,8 +13,12 @@ function App() {
   return (
     <>
      {/* <ex1 />
-     <Ex2 /> */}
+     <Ex2 />
     <Ex3 />
+    <ex4 /> */}
+    {/* <DetailPost /> */}
+    <Ex7 />
+    {/* <Ex8 /> */}
     </>
   )
 }
